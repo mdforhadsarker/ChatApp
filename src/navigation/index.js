3469eaf08b2038a868414d453cce8d265cfa867b
Md Forhad Sarkar <news9097@gmail.com>
2020-04-09 12:50:37 +0800

    initial commit

---
+++ src/navigation/index.js
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+import React from 'react';
+import {Provider as PaperProvider} from 'react-native-paper';
+import Routes from './Routes';
+
+
+export default function Providers() {
+  return (
+    <PaperProvider>
+            <Routes />
+    </PaperProvider>
+  );
+}
